8b612513ec11ba8d3b2142a7848dac98cfb30c45
maharjanshreya <Np03cs4s220117@heraldcollege.edu.np>
2023-12-15 17:41:19 +0545

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const imageStyle = {
+    width: '300px', // Set the desired width
+    height: '200px', // Set the desired height
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      {/*<header className="App-header">
+         <img src={logo} className="App-logo" alt="logo" /> 
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
+      </header>*/}
+      <h1>Photo Frames</h1>
+      <div className='flex-container'>
+        <div className="flex-item"><img src="https://www.ikea.com/us/en/images/products/silverhoejden-frame-gold__0638523_pe698987_s5.jpg" alt="A sample image" style={imageStyle} /></div>
+        <div className="flex-item"><img src="https://www.ikea.com/us/en/images/products/silverhoejden-frame-gold__0638523_pe698987_s5.jpg" alt="A sample image" style={imageStyle} /></div>
+        <div className="flex-item"><img src="https://www.ikea.com/us/en/images/products/silverhoejden-frame-gold__0638523_pe698987_s5.jpg" alt="A sample image" style={imageStyle} /></div>
+
+      </div>
+
     </div>
   );
 }
