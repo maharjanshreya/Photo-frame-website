2bc5776dac9fa070cdca42d5a0b0417c06873bef
maharjanshreya <Np03cs4s220117@heraldcollege.edu.np>
2023-12-19 22:24:59 +0545

    Login Page

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
 import './App.css';
-
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './Homepage';
+import Login from './LoginRegister/Login';
+import myImage from './Images/Rectangle 1.png';
 function App() {
-  const imageStyle = {
-    width: '300px', // Set the desired width
-    height: '200px', // Set the desired height
-  };
+ 
 
   return (
-    <div className="App">
-      {/*<header className="App-header">
-         <img src={logo} className="App-logo" alt="logo" /> 
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-      </header>*/}
-      <h1>Photo Frames</h1>
-      <div className='flex-container'>
-        <div className="flex-item"><img src="https://www.ikea.com/us/en/images/products/silverhoejden-frame-gold__0638523_pe698987_s5.jpg" alt="A sample image" style={imageStyle} /></div>
-        <div className="flex-item"><img src="https://www.ikea.com/us/en/images/products/silverhoejden-frame-gold__0638523_pe698987_s5.jpg" alt="A sample image" style={imageStyle} /></div>
-        <div className="flex-item"><img src="https://www.ikea.com/us/en/images/products/silverhoejden-frame-gold__0638523_pe698987_s5.jpg" alt="A sample image" style={imageStyle} /></div>
-        <p>Checking the updates</p>
-      </div>
-
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/LoginRegister/Login.js
@@ -0,0 +1,25 @@
+import sideImage from '../Images/LoginImage.png';
+import Logo from '../Images/Rectangle 1.png';
+import  '../CSS/style.css';
+function Login(){
+    return(
+        <div className="full-screen-image">
+            <div className="left-column">
+                <img src={sideImage} alt="logout" className="full-height-image"  />
+            </div>
+            <div className="right-column">
+                <p className='text'>Login and <br/>Design your frames with</p>
+                <img src={Logo} alt="logout" className=""  />
+                <form>
+                <input type="text" id="username" name="username" placeholder="Username" /><br/>
+                <input type="text" id="password" name="password" placeholder="Password" /><br/>
+                <input type="submit" value="Login  " />
+                </form>
+                <p>Don't have an account? <u className='register-color'>Register Now </u></p>
+            </div>
+            
+        
+        </div>
+    );
+}
+export default Login;
