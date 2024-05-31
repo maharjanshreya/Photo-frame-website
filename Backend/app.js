85c4b41032e1a9ac4955e8afe26161241a8652d8
maharjanshreya <Np03cs4s220117@heraldcollege.edu.np>
2024-05-31 16:01:18 +0545

    Hosting issue 3

--- Backend/app.js
+++ Backend/app.js
@@ -30,7 +30,7 @@ db.once('open', function() {
 });
 // Enable CORS for all routes
 app.use(cors({
-  origin: 'https://photo-frame-website.vercel.app', 
+  origin: 'https://samanphotoframe.vercel.app', 
   credentials: true,
   exposedHeaders: ['set-cookie'],
 }));
